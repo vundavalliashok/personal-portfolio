fdd499ba2d81ab5433a6fdafe8a7f75857980375
Ashok Vundavalli <vundavalliashok@gmail.com>
2026-09-03 14:18:08 +0530

    first commint

---
+++ app.py
@@ -0,0 +1,13 @@
+# pyright: reportMissingImports=false
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
